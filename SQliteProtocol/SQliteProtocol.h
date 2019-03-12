db7f2c9e4daab66a63e64297b662daeff0bd2ef6
Manuel Reich <mr@wundf.net>
2019-03-12 14:31:15 +0100

    Initial Commit

---
+++ SQliteProtocol/SQliteProtocol.h
@@ -0,0 +1,19 @@
+//
+//  SQliteProtocol.h
+//  SQliteProtocol
+//
+//  Created by Manuel Reich on 12.03.19.
+//  Copyright © 2019 dcb-ug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQliteProtocol.
+FOUNDATION_EXPORT double SQliteProtocolVersionNumber;
+
+//! Project version string for SQliteProtocol.
+FOUNDATION_EXPORT const unsigned char SQliteProtocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQliteProtocol/PublicHeader.h>
+
+
