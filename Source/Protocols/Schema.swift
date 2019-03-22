5edf36e38e7e86b2e985899debbc515665660547
Manuel Reich <mr@wundf.net>
2019-03-22 14:40:29 +0100

    Rename Columnbuilder

--- Source/Protocols/Schema.swift
+++ Source/Protocols/Schema.swift
@@ -7,16 +7,6 @@
 
 import SQLite
 
-public struct PrimaryKey<Schema, KeyType> {
-    public let name: String
-    public let path: KeyPath<Schema, KeyType>
-
-    public init(name: String, keyPath: KeyPath<Schema, KeyType>) {
-        self.name = name
-        self.path = keyPath
-    }
-}
-
 public protocol Schema {
     associatedtype Model
     associatedtype PrimaryKeyType: Value where PrimaryKeyType.Datatype: Equatable
@@ -33,13 +23,12 @@ public protocol Schema {
 }
 
 extension Schema {
-
-    public static func columnBuilder<Property: Value>(path: WritableKeyPath<Self, Property?>, name: String) -> Column {
-        let expression = Expression<Property?>(name)
+    public static func build<Property: Value>(column name: String, keyPath: WritableKeyPath<Self, Property>) -> Column {
+        let expression = Expression<Property>(name)
 
         let setterBuilder = {(model: Model) -> Setter in
             let schema = Self(model: model)
-            let value = schema[keyPath: path]
+            let value = schema[keyPath: keyPath]
             return expression <- value
         }
 
@@ -48,15 +37,33 @@ extension Schema {
         let addColumnValue = { (row: Row, columns: Self) -> Self in
             var columns = columns
             let value = row[expression]
-            columns[keyPath: path] = value
+            columns[keyPath: keyPath] = value
             return columns
         }
 
         return (setterBuilder, columnBuilder, addColumnValue)
     }
 
-    public static func columnBuilder<Property: Value>(path: WritableKeyPath<Self, Property>, name: String) -> Column {
-        return columnBuilder(path: path, name: name)
+    // TODO: this must be possible without writeing everything twice
+    public static func build<Property: Value>(column name: String, keyPath: WritableKeyPath<Self, Property?>) -> Column {
+        let expression = Expression<Property?>(name)
+
+        let setterBuilder = {(model: Model) -> Setter in
+            let schema = Self(model: model)
+            let value = schema[keyPath: keyPath]
+            return expression <- value
+        }
+
+        let columnBuilder = { (tableBuilder: TableBuilder) in tableBuilder.column(expression) }
+
+        let addColumnValue = { (row: Row, columns: Self) -> Self in
+            var columns = columns
+            let value = row[expression]
+            columns[keyPath: keyPath] = value
+            return columns
+        }
+
+        return (setterBuilder, columnBuilder, addColumnValue)
     }
 
     public static func buildTable(tableBuilder: TableBuilder) {
@@ -74,12 +81,12 @@ extension Schema {
     }
 
     public static func primaryKeySelector(value: PrimaryKeyType) -> Expression<Bool> {
-        let expression = Expression<PrimaryKeyType>(Self.primaryKey.name)
+        let expression = Expression<PrimaryKeyType>(Self.primaryKey.column)
         return expression == value
     }
 
     public var primaryKeySelector: Expression<Bool> {
-        let expression = Expression<PrimaryKeyType>(Self.primaryKey.name)
+        let expression = Expression<PrimaryKeyType>(Self.primaryKey.column)
         let value = self[keyPath: Self.primaryKey.path]
         return expression == value
     }
