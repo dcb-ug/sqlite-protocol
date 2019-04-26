e6d6dde2646ed4a906b29b8cbc7ec3ba03d4127c
Manuel Reich <mr@wundf.net>
2019-04-26 10:13:06 +0200

    Add correct name for PrimaryColumn

--- Source/Models/PrimaryColumn.swift
+++ Source/Models/PrimaryColumn.swift
@@ -1,20 +1,10 @@
 //
-//  Column.swift
+//  PrimaryColumn.swift
 //  SQLiteProtocol
 //
 //  Created by Manuel Reich on 22.03.19.
 //
 import SQLite
-
-//public struct Column<Schema, ValueType> {
-//    public let name: String
-//    public let path: KeyPath<Schema, ValueType>
-//
-//    public init(name: String, keyPath: KeyPath<Schema, ValueType>) {
-//        self.name = name
-//        self.path = keyPath
-//    }
-//}
 
 public struct PrimaryColumn<Model, Columns: ColumnSchema, KeyType: Value> {
     private let mapper: (Model) throws -> KeyType
