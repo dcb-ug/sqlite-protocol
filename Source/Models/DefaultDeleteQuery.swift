116a1a50610e9c36462574bd521dfe51474b03c0
Manuel Reich <mr@wundf.net>
2019-04-29 14:33:56 +0200

    Improve naming of _delete helper function

--- Source/Models/DefaultDeleteQuery.swift
+++ Source/Models/DefaultDeleteQuery.swift
@@ -10,14 +10,14 @@ import SQLite
 public final class DefaultDeleteQuery<Model: Persistable>: DeleteQueryProtocol {
 
     /// this is exposed like this so you can use it when building your own queries
-    public static func _delete(with primaryKey: Model.Columns.PrimaryKeyType, connection: Connection) throws {
+    public static func _delete(wherePrimaryKey primaryKey: Model.Columns.PrimaryKeyType, connection: Connection) throws {
         let primaryKeySelector = Model.Columns.primaryKeySelector(value: primaryKey)
         let query = DefaultDeleteQuery.table.filter(primaryKeySelector)
         try connection.run(query.delete())
     }
 
     /// this is exposed like this so you can use it when building your own queries
-    public static func _delete(with primaryKeys: [Model.Columns.PrimaryKeyType], connection: Connection) throws {
+    public static func _delete(wherePrimaryKeys primaryKeys: [Model.Columns.PrimaryKeyType], connection: Connection) throws {
         let primaryKeyExpression = Expression<Model.Columns.PrimaryKeyType>(Model.Columns.primaryColumn.name)
         let query = DefaultDeleteQuery.table.filter(primaryKeys.contains(primaryKeyExpression))
         try connection.run(query.delete())
@@ -31,27 +31,27 @@ public final class DefaultDeleteQuery<Model: Persistable>: DeleteQueryProtocol {
 
     public static func `where`(primaryKey: Model.Columns.PrimaryKeyType) -> DefaultDeleteQuery {
         return DefaultDeleteQuery { connection in
-            try DefaultDeleteQuery._delete(with: primaryKey, connection: connection)
+            try DefaultDeleteQuery._delete(wherePrimaryKey: primaryKey, connection: connection)
         }
     }
 
     public static func `where`(primaryKeys: [Model.Columns.PrimaryKeyType]) -> DefaultDeleteQuery {
         return DefaultDeleteQuery { connection in
-            try DefaultDeleteQuery._delete(with: primaryKeys, connection: connection)
+            try DefaultDeleteQuery._delete(wherePrimaryKeys: primaryKeys, connection: connection)
         }
     }
 
     public static func model(_ model: Model) -> DefaultDeleteQuery {
         return DefaultDeleteQuery { connection in
             let primaryKey = try Model.Columns.primaryColumn.value(from: model)
-            try DefaultDeleteQuery._delete(with: primaryKey, connection: connection)
+            try DefaultDeleteQuery._delete(wherePrimaryKey: primaryKey, connection: connection)
         }
     }
 
     public static func models(_ models: [Model]) -> DefaultDeleteQuery {
         return DefaultDeleteQuery { connection in
             let primaryKeys = try models.map { model in try Model.Columns.primaryColumn.value(from: model) }
-            try DefaultDeleteQuery._delete(with: primaryKeys, connection: connection)
+            try DefaultDeleteQuery._delete(wherePrimaryKeys: primaryKeys, connection: connection)
         }
     }
 
