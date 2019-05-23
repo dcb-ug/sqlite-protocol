cf080de8923b166c9f3d7e9fe4572735d2e8f20b
Manuel Reich <mr@wundf.net>
2019-05-23 13:38:44 +0200

    Swallow no such table error for read queries

--- Source/Models/DefaultReadQuery.swift
+++ Source/Models/DefaultReadQuery.swift
@@ -10,16 +10,19 @@ import SQLite
 public final class DefaultReadQuery<Model: Persistable>: ReadQueryProtocol {
     public typealias PrimaryKeyType = Model.Columns.PrimaryKeyType
 
-    public static var first: DefaultReadQuery {
-        return DefaultReadQuery { connection in
+    /// this is exposed like this so you can use it when building your own queries
+    public static func _readFirst (using connection: Connection) throws -> [Model] {
+        return try swallowNoSuchTableError {
             guard let row = try connection.pluck(DefaultReadQuery.table) else { return [] }
             let columns = Model.Columns.from(row: row)
             return try [Model(databaseColumns: columns)]
         }
     }
 
-    public static func withPrimaryKey(_ key: Model.Columns.PrimaryKeyType) -> DefaultReadQuery {
-        return DefaultReadQuery { connection in
+    /// this is exposed like this so you can use it when building your own queries
+    public static func _read (withPrimaryKey key: Model.Columns.PrimaryKeyType,
+                              using connection: Connection) throws -> [Model] {
+        return try swallowNoSuchTableError {
             let query = DefaultReadQuery.table.filter(Model.Columns.primaryKeySelector(value: key))
             guard let row = try connection.pluck(query) else { return [] }
             let columns = Model.Columns.from(row: row)
@@ -27,14 +30,36 @@ public final class DefaultReadQuery<Model: Persistable>: ReadQueryProtocol {
         }
     }
 
-    public static var all: DefaultReadQuery {
-        return DefaultReadQuery { connection in
+    /// this is exposed like this so you can use it when building your own queries
+    public static func _readAll (using connection: Connection) throws -> [Model] {
+        return try swallowNoSuchTableError {
             let rows = try connection.prepare(DefaultReadQuery.table)
             return try rows.map { row -> Model in
                 let columns = Model.Columns.from(row: row)
                 return try Model(databaseColumns: columns)
             }
         }
+    }
+
+    /// this is exposed like this so you can use it when building your own queries
+    public static func swallowNoSuchTableError(function: () throws -> [Model]) rethrows -> [Model] {
+        do {
+            return try function()
+        } catch let Result.error(message, code, _) where code == SQLITE_ERROR && message.hasPrefix("no such table") {
+            return []
+        }
+    }
+
+    public static var first: DefaultReadQuery {
+        return DefaultReadQuery { connection in try _readFirst(using: connection) }
+    }
+
+    public static func withPrimaryKey(_ key: Model.Columns.PrimaryKeyType) -> DefaultReadQuery {
+        return DefaultReadQuery { connection in try _read(withPrimaryKey: key, using: connection) }
+    }
+
+    public static var all: DefaultReadQuery {
+        return DefaultReadQuery { connection in try _readAll(using: connection) }
     }
 
     private let block: (Connection) throws -> [Model]
